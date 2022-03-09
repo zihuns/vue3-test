5641e1a51100475be6d4c461553a5810637ded9e
zihuns <seojh44@uos.ac.kr>
2022-03-09 09:58:40 +0900

    vue3-test

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1 @click="increase">
+    {{ count }}
+  </h1>
+  <div v-if="count > 4">
+    4보다 큽니다!
+  </div>
+  <ul>
+    <Fruit
+      v-for="fruit in fruits"
+      :key="fruit"
+      :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
+</template>
+
+<script>
+import Fruit from '~/components/Fruit'
+
+export default {
+  components: {
+    Fruit
+  },
+  data() {
+    return {
+      count: 0,
+      fruits: ['Apple', 'Banana', 'Cherry']
+    }
+  },
+  methods: {
+    increase() {
+      this.count += 1
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  h1 {
+    font-size: 50px;
+    color: royalblue;
+  }
+  ul{
+    li {
+      font-size: 40px;
+    }
+  }
+</style>
